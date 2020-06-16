c2ee6e6ac284aa580feb9b7112a1f0dbea33ebed
axchu <achu.umich@gmail.com>
2020-06-15 20:26:38 -0500

    flesh out fcns & tests
    tests pass, more work needed on TestBlockServer

--- oserver.go
+++ oserver.go
@@ -5,84 +5,164 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"sync"
+	"time"
 )
 
-var BLOCKSIZE = int64(5)
+var BLOCKSIZE = 5
 
 type memPoolDB struct {
 	sync.Mutex
 	mempool   []*Transaction
-	blockSize int64
+	blockSize int
 }
-
-type block []*Transaction
 
 type Transaction struct {
 	Message  string `json:"message"`
 	GasPrice int64  `json:"gasprice"`
 }
 
-func newMemPoolDB() *memPoolDB {
+func newMemPoolDB(blockSize int) *memPoolDB {
 	mpDB := new(memPoolDB)
 	mpDB.mempool = []*Transaction{}
-	mpDB.blockSize = BLOCKSIZE
+	mpDB.blockSize = blockSize
 	return mpDB
 }
 
 func (mpDB *memPoolDB) insert(txn *Transaction) {
 	mpDB.Lock()
 	defer mpDB.Unlock()
-
+	mpDB.mempool = append(mpDB.mempool, txn)
 }
 
-func (mpDB *memPoolDB) delete(txn *Transaction) {
-	mpDB.Lock()
-	defer mpDB.Unlock()
-}
+// to use pkg sort's nlogn optimized quicksort
+type ByGasPrice []*Transaction
 
-func (mpDB *memPoolDB) sortGreatestFirst() (err error) {
+func (gpList ByGasPrice) Len() int           { return len(gpList) }
+func (gpList ByGasPrice) Less(i, j int) bool { return gpList[i].GasPrice < gpList[j].GasPrice }
+func (gpList ByGasPrice) Swap(i, j int)      { gpList[i], gpList[j] = gpList[j], gpList[i] }
+
+func (mpDB *memPoolDB) sortDescending() (err error) {
 	mpDB.Lock()
 	defer mpDB.Unlock()
+
+	sort.Sort(sort.Reverse(ByGasPrice(mpDB.mempool)))
+
 	return nil
 }
 
-func (mpDB *memPoolDB) makeBlock() (blk *block, err error) {
-	//mpDB.Lock()
-	//defer mpDB.Unlock()
+func (mpDB *memPoolDB) makeBlock() (block []*Transaction) {
+	mpDB.sortDescending()
 
-	// sort greatest first
+	mpDB.Lock()
+	defer mpDB.Unlock()
 
-	// copy top [0:blockSize] out, delete it from the mempool
-
-	return blk, nil
-}
-
-func StringToTransaction(message string) (txn Transaction) {
-	err := json.Unmarshal([]byte(message), &txn)
-	if err != nil {
-		log.Fatal(err)
+	blockLength := 0
+	memPoolLength := len(mpDB.mempool)
+	switch {
+	case memPoolLength == 0:
+		//log.Printf("memPoolDB:makeBlock nothing in mempool. No block made.")
+		blockLength = 0
+	case memPoolLength < mpDB.blockSize:
+		blockLength = memPoolLength
+	default:
+		blockLength = mpDB.blockSize
 	}
-	return txn
+	//dprint("mempool")
+	//dprint(BlockToString(mpDB.mempool))
+	blk := mpDB.mempool[0:blockLength]
+	//dprint("block")
+	//dprint(BlockToString(blk))
+	mpDB.mempool = mpDB.mempool[blockLength:memPoolLength]
+	//dprint("new mempool")
+	//dprint(BlockToString(mpDB.mempool))
+	return blk
 }
 
-func main() {
-
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
+func (mpDB *memPoolDB) BlockServer(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-	log.Printf("%s\n", body)
+	txn, err := MessageToTransaction(body)
+	if err != nil {
+		http.Error(w, "message "+string(body)+" is not transaction", http.StatusBadRequest)
+		return
+	}
+	mpDB.insert(&txn)
+	log.Printf("inserted(%+v)\n", txn)
+	// ctx := r.Context()
+	// select {
+	// case <-time.After(3 * time.Second):
+	//
+	// case <-ctx.Done():
+	// 	err := ctx.Err()
+	// 	fmt.Println("server:", err)
+	// 	internalError := http.StatusInternalServerError
+	// 	http.Error(w, err.Error(), internalError)
+	// }
+}
 
-	// add txn to mempool
+func MessageToTransaction(message []byte) (txn Transaction, err error) {
+	err = json.Unmarshal(message, &txn)
+	// if err != nil {
+	// 	log.Printf("ERROR MessageToTransaction %v", err)
+	// }
+	return txn, err
+}
 
-	// every 3 seconds, make a block; write the block to log and the
+func TransactionToMessage(txn Transaction) (message []byte, err error) {
+	message, err = json.Marshal(txn)
+	// if err != nil {
+	// 	log.Printf("ERROR TransactionToMessage %v", err)
+	// }
+	return message, err
+}
+
+func Init() {
+
+	mpDB := newMemPoolDB(BLOCKSIZE)
+	go func() {
+		log.Printf("server coming up\n")
+		http.HandleFunc("/", mpDB.BlockServer)
+		http.ListenAndServe(":8080", nil)
+	}()
+
+	go func() {
+		log.Printf("making block goroutine spawned\n")
+		for {
+			//time.Sleep(3 * time.Second)
+			time.Sleep(time.Millisecond * 100)
+			block := mpDB.makeBlock()
+			blockString := BlockToString(block)
+			log.Printf("block made(%v)", blockString)
+		}
+	}()
 
 }
+
+func main() {
+	Init()
+}
+
+func BlockToString(block []*Transaction) (str string) {
+	str = ""
+	for _, txn := range block {
+		msg, err := TransactionToMessage(*txn)
+		if err != nil {
+			panic(err)
+		}
+		str += "[" + string(msg) + "]\n"
+	}
+	return str
+}
+
+// func dprint(in string, args ...interface{}) {
+// 	if in == "\n" {
+// 		fmt.Println()
+// 	} else {
+// 		fmt.Printf("[debug] "+in+"\n", args...)
+// 	}
+// }
--- oserver_test.go
+++ oserver_test.go
@@ -2,8 +2,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,7 +21,7 @@ do
        echo "Sending a message at: $(($(gdate +%s%N)/1000000))"
        curl --header "Content-Type: application/json" \
            --request POST \
-           --data '{"message": "xyz","gasPrice": 5000}' \
+           --data '{"message": "xyz","gasprice": 5000}' \
            http://localhost:8080/omessage
        echo ""
        nloops=$((nloops+1))
@@ -24,64 +29,190 @@ do
 done
 */
 
-var INPUTS = map[int]string{
-	0: "{\"message\": \"xyz\",\"gasPrice\": 5000}",
-	//...
-}
-
-//{"message": "xyz","gasPrice": 5000}
+//{"message": "xyz","gasprice": 5000}
 func TestInsert(t *testing.T) {
-	mpDB := newMemPoolDB()
-	for _, input := range INPUTS {
-		txn := StringToTransaction(input)
+	mpDB := newMemPoolDB(TEST_BLOCKSIZE)
+	for _, input := range TEST_INPUTS {
+		txn, err := MessageToTransaction([]byte(input))
+		require.NoError(t, err)
 		mpDB.insert(&txn)
 	}
-	for _, input := range INPUTS {
-		// search and find each expected txn in mempool
+
+	// check
+	if len(mpDB.mempool) != len(TEST_INPUTS) {
+		t.Fatalf("mempool has (%v) != (%v)", len(mpDB.mempool), len(TEST_INPUTS))
+	}
+	matched := 0
+	for _, actual := range mpDB.mempool {
+		actualmsg, err := TransactionToMessage(*actual)
+		require.NoError(t, err)
+		//tprint("actualmsg(%s)", string(actualmsg))
+		for _, expected := range TEST_INPUTS {
+			//tprint("expected(%s)", string(expected))
+			if string(actualmsg) == expected {
+				matched++
+				break
+			}
+		}
+	}
+	if len(TEST_INPUTS) != matched {
+		t.Fatalf("Insert Failed, expected(%v) != matched(%v)", len(TEST_INPUTS), matched)
 	}
 }
 
-func TestDelete(t *testing.T) {
-	mpDB := newMemPoolDB()
-	for _, input := range INPUTS {
-		txn := StringToTransaction(input)
+func TestSortDescending(t *testing.T) {
+	mpDB := newMemPoolDB(TEST_BLOCKSIZE)
+	for _, input := range TEST_INPUTS {
+		txn, err := MessageToTransaction([]byte(input))
+		require.NoError(t, err)
 		mpDB.insert(&txn)
 	}
-	toDelete := map[int]string{
-		0: "{\"message\": \"xyz\",\"gasPrice\": 5000}",
-		// ...
-	}
-	for _, item := range toDelete {
-		txn := StringToTransaction(item)
-		mpDB.delete(&txn)
-	}
-	// require.DeepEquals expected txn list in mempool
-}
+	require.NoError(t, mpDB.sortDescending())
 
-func TestSortGreatestFirst(t *testing.T) {
-	mpDB := newMemPoolDB()
-	for _, input := range INPUTS {
-		txn := StringToTransaction(input)
-		mpDB.insert(&txn)
+	// check
+	for i, expected := range TEST_SORTEDINPUTS {
+		actual := mpDB.mempool[i]
+		actualmsg, err := TransactionToMessage(*actual)
+		require.NoError(t, err)
+		if expected != string(actualmsg) {
+			t.Fatalf("SortDescending Failed expected(%v) actual(%v)", expected, string(actualmsg))
+		}
 	}
-	require.NoError(t, mpDB.sortGreatestFirst())
-	// require.DeepEquals expected list
-
-	// error case?
-
 }
 
 func TestMakeBlock(t *testing.T) {
-	mpDB := newMemPoolDB()
-	for _, input := range INPUTS {
-		txn := StringToTransaction(input)
+	mpDB := newMemPoolDB(TEST_BLOCKSIZE)
+	for _, input := range TEST_INPUTS {
+		txn, err := MessageToTransaction([]byte(input))
+		require.NoError(t, err)
 		mpDB.insert(&txn)
 	}
-	blk, err := mpDB.makeBlock()
-	require.NoError(t, err)
-	// check block
-	// check mempool left
+	blk := mpDB.makeBlock()
+	//tprint(BlockToString(blk))
+	// check
+	if len(blk) != TEST_BLOCKSIZE {
+		t.Fatalf("MakeBlock failed, blocksize (%v) is not the right size (%v)", len(blk), TEST_BLOCKSIZE)
+	}
+	if len(mpDB.mempool) != len(TEST_RESTOFMEMPOOL) {
+		t.Fatalf("MakeBlock failed, mempool size (%v) is not the right size (%v)", len(mpDB.mempool), len(TEST_RESTOFMEMPOOL))
+	}
 
-	// error case?
+	for i, expected := range TEST_BLOCK {
+		actual := blk[i]
+		actualmsg, err := TransactionToMessage(*actual)
+		require.NoError(t, err)
+		if expected != string(actualmsg) {
+			t.Fatalf("MakeBlock Failed expected(%v) actual(%v)", expected, string(actualmsg))
+		}
+	}
+	for i, expected := range TEST_RESTOFMEMPOOL {
+		actual := mpDB.mempool[i]
+		actualmsg, err := TransactionToMessage(*actual)
+		require.NoError(t, err)
+		if expected != string(actualmsg) {
+			t.Fatalf("MakeBlock mempool Failed expected(%v) actual(%v)", expected, string(actualmsg))
+		}
+	}
 
+	// check smaller block
+	mpDB.blockSize = 10
+	smallBlock := mpDB.makeBlock()
+	assert.Equal(t, len(smallBlock), len(TEST_RESTOFMEMPOOL))
+	assert.Equal(t, 0, len(mpDB.mempool))
+
+	// check empty mempool
+	mpDB.mempool = make([]*Transaction, 0)
+	emptyBlock := mpDB.makeBlock()
+	assert.Equal(t, 0, len(emptyBlock))
+
+}
+
+func TestBlockServer(t *testing.T) {
+
+	Init()
+
+	for _, input := range TEST_INPUTS {
+		time.Sleep(time.Millisecond * 50)
+		req, err := http.NewRequest("POST", "http://localhost:8080", bytes.NewBuffer([]byte(input)))
+		if err != nil {
+			t.Fatalf("Error reading request. %v", err)
+		}
+
+		// Set headers
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		// Check the status code is what we expect.
+		//if status := rr.Code; status != http.StatusOK {
+		//	t.Errorf("handler returned wrong status code: got %v want %v",
+		//		status, http.StatusOK)
+		///}
+		tprint("sent req(%v)", input)
+	}
+	// Check the response body is what we expect.
+	// expected := `{"alive": true}`
+	// if rr.Body.String() != expected {
+	// 	t.Errorf("handler returned unexpected body: got %v want %v",
+	// 		rr.Body.String(), expected)
+	// }
+
+}
+
+var TEST_INPUTS = []string{
+	0:  "{\"message\":\"xyz\",\"gasprice\":5000}",
+	1:  "{\"message\":\"hello\",\"gasprice\":40}",
+	2:  "{\"message\":\"abc\",\"gasprice\":5050}",
+	3:  "{\"message\":\"222\",\"gasprice\":5001}",
+	4:  "{\"message\":\"this gas price is the best\",\"gasprice\":0}",
+	5:  "{\"message\":\"avery\",\"gasprice\":22}",
+	6:  "{\"message\":\"myTransaction\",\"gasprice\":800}",
+	7:  "{\"message\":\"alice 234\",\"gasprice\":100000}",
+	8:  "{\"message\":\"123 bob\",\"gasprice\":1}",
+	9:  "{\"message\":\"aaaaaaa\",\"gasprice\":16}",
+	10: "{\"message\":\"whodis\",\"gasprice\":333}",
+}
+
+var TEST_BLOCKSIZE = 5
+var TEST_BLOCK = []string{
+	0: "{\"message\":\"alice 234\",\"gasprice\":100000}",
+	1: "{\"message\":\"abc\",\"gasprice\":5050}",
+	2: "{\"message\":\"222\",\"gasprice\":5001}",
+	3: "{\"message\":\"xyz\",\"gasprice\":5000}",
+	4: "{\"message\":\"myTransaction\",\"gasprice\":800}",
+}
+
+var TEST_RESTOFMEMPOOL = []string{
+	0: "{\"message\":\"whodis\",\"gasprice\":333}",
+	1: "{\"message\":\"hello\",\"gasprice\":40}",
+	2: "{\"message\":\"avery\",\"gasprice\":22}",
+	3: "{\"message\":\"aaaaaaa\",\"gasprice\":16}",
+	4: "{\"message\":\"123 bob\",\"gasprice\":1}",
+	5: "{\"message\":\"this gas price is the best\",\"gasprice\":0}",
+}
+
+var TEST_SORTEDINPUTS = []string{
+	0:  "{\"message\":\"alice 234\",\"gasprice\":100000}",
+	1:  "{\"message\":\"abc\",\"gasprice\":5050}",
+	2:  "{\"message\":\"222\",\"gasprice\":5001}",
+	3:  "{\"message\":\"xyz\",\"gasprice\":5000}",
+	4:  "{\"message\":\"myTransaction\",\"gasprice\":800}",
+	5:  "{\"message\":\"whodis\",\"gasprice\":333}",
+	6:  "{\"message\":\"hello\",\"gasprice\":40}",
+	7:  "{\"message\":\"avery\",\"gasprice\":22}",
+	8:  "{\"message\":\"aaaaaaa\",\"gasprice\":16}",
+	9:  "{\"message\":\"123 bob\",\"gasprice\":1}",
+	10: "{\"message\":\"this gas price is the best\",\"gasprice\":0}",
+}
+
+func tprint(in string, args ...interface{}) {
+	if in == "\n" {
+		fmt.Println()
+	} else {
+		fmt.Printf("[test] "+in+"\n", args...)
+	}
 }
